4f6122dedb855991c6954d1821712f313e2f16da
park2002js <60747783+park2002js@users.noreply.github.com>
2024-11-07 20:48:30 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,34 @@
 #include <time.h>
 #include "array_poly.h"
 
+/*-------- 추가한 부분 --------- */
+void test_ArrayPoly_A(ArrayPoly *a){
+    int coef[] = {1,-2,3,-4,5};
+    for(int i=0; i< 5 ; i++ ){
+        a->coef[i] = coef[i];
+    }
+}
+
+void test_ArrayPoly_B(ArrayPoly *a){
+    int coef[] = {5,-3,2,-1,4};
+    for(int i=0; i< 5 ; i++ ){
+        a->coef[i] = coef[i];
+    }
+}
+/*------------------------------ */
+
 int main(){
     srand(time(NULL));
 
     ArrayPoly *a = createArrayPoly(100000);
     ArrayPoly *b = createArrayPoly(1500000);
 
-    test_randArrayPoly(a,5);
+    test_ArrayPoly_A(a);
     // randArrayPoly(a);
     printf("f(x) = ");
     printArrayPoly(a);
 
-    test_randArrayPoly(b,5);
+    test_ArrayPoly_B(b);
     //randArrayPoly(b);
     printf("g(x) = ");
     printArrayPoly(b);
