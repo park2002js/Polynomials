e62107d3649b692e9d33a2fd0d780399d71ab1dd
park2002js <60747783+park2002js@users.noreply.github.com>
2024-11-07 20:29:18 +0900

    Update array_poly.h

--- array_poly.h
+++ array_poly.h
@@ -13,4 +13,7 @@ void printArrayPoly(ArrayPoly*);
 ArrayPoly* addArrayPoly(ArrayPoly*, ArrayPoly*);
 ArrayPoly* subArrayPoly(ArrayPoly*, ArrayPoly*);
 
+// for test
+void test_randArrayPoly(ArrayPoly *, int);
+
 #endif
